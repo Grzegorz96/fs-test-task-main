abae3a2b976b5b026b389028d599feccca1b73a4
Grzegorz Strzeszewski <grzesstrzeszewski@gmail.com>
2025-12-14 18:10:11 +0100

    chore: configured docker for dev and prod
    - added .dockerignore
    - added Dockerfile and Dockerfile.dev
    - added docker-compose.dev.yml

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,10 +1,18 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/test-task";
 
 app.use(express.json());
 
+app.get("/health", (req: Request, res: Response) => {
+  res.status(200).json({ status: "ok", mongodb: MONGODB_URI });
+});
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Serversssssss running on http://localhost:${PORT}`);
+  console.log(`MongoDB URI: ${MONGODB_URI}`);
 });
